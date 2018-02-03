774a54eae269d9d41825b9604e47750caa7daa76
Peter Iontsev <sninja89@gmail.com>
2018-02-03 07:40:41 +0200

    calculator 1st revision

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -1,25 +1,89 @@
-//
-//  ViewController.swift
-//  calc
-//
-//  Created by Peter Iontsev on 1/27/18.
-//  Copyright © 2018 Peter Iontsev. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+enum Operation: String {
+    case Add = "+"
+    case Subtract = "-"
+    case Multiply = "*"
+    case Divide = "/"
+    case NULL = "Null"
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var runningNumber = ""
+    var leftValue = ""
+    var rightValue = ""
+    var result = ""
+    var currentOperation: Operation = .NULL
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        outputLabel.text = "0"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if runningNumber.count <= 8 {
+        runningNumber += "\(sender.tag)"
+        outputLabel.text = runningNumber
+        }
     }
-
-
+    
+    @IBAction func allClearPressed(_ sender: UIButton) {
+        runningNumber = ""
+        leftValue = ""
+        rightValue = ""
+        result = ""
+        currentOperation = .NULL
+        outputLabel.text = "0"
+    }
+    @IBAction func dotPressed(_ sender: UIButton) {
+        if runningNumber.count <= 7 {
+        runningNumber += "."
+        outputLabel.text = runningNumber
+        }
+    }
+    @IBAction func equalPressed(_ sender: UIButton) {
+        operation(operation: currentOperation)
+    }
+    @IBAction func addPressed(_ sender: UIButton) {
+        operation(operation: .Add)
+    }
+    @IBAction func subtractPressed(_ sender: UIButton) {
+        operation(operation: .Subtract)
+    }
+    @IBAction func multiplyPressed(_ sender: UIButton) {
+        operation(operation: .Multiply)
+    }
+    @IBAction func dividePressed(_ sender: UIButton) {
+        operation(operation: .Divide)
+    }
+    
+    func operation (operation: Operation) {
+        if currentOperation != .NULL {
+            if runningNumber != "" {
+                rightValue = runningNumber
+                runningNumber = ""
+                if currentOperation == .Add {
+                    result = "\(Double(leftValue)! + Double(rightValue)!)"
+                } else if currentOperation == .Subtract {
+                    result = "\(Double(leftValue)! - Double(rightValue)!)"
+                } else if currentOperation == .Multiply {
+                    result = "\(Double(leftValue)! * Double(rightValue)!)"
+                } else if currentOperation == .Divide {
+                    result = "\(Double(leftValue)! / Double(rightValue)!)"
+                }
+                leftValue = result
+                outputLabel.text = result
+            }
+            currentOperation = operation
+        } else {
+            leftValue = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
+    
 }
 
